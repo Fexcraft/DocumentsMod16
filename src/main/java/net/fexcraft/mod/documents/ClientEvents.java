4b7b22c8b6c1d2972db5b4025dc0744f3d249484
Ferdinand Calo <fexcalo@gmail.com>
2024-01-09 22:28:48 +0100

    Update ClientEvents.java

--- src/main/java/net/fexcraft/mod/documents/ClientEvents.java
+++ src/main/java/net/fexcraft/mod/documents/ClientEvents.java
@@ -1,13 +1,27 @@
 package net.fexcraft.mod.documents;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
 import net.fexcraft.mod.documents.gui.DocEditorScreen;
 import net.fexcraft.mod.documents.gui.DocViewerScreen;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.renderer.model.*;
+import net.minecraft.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.model.IModelConfiguration;
+import net.minecraftforge.client.model.IModelLoader;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.model.geometry.IModelGeometry;
+import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.function.Function;
 
 @EventBusSubscriber(modid = "documents", bus = Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
